c108565a7cbc129fdc567f7c389cda88e8e5a820
luyvannda <philips.joe329@gmail.com>
2024-01-21 11:15:56 +0700

    use grid to make a responsive web page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,42 @@
 export default function Hero() {
   return (
     <div>
-      <div className="px-8 py-12">
-        <img className="h-10" src="/logo.svg" alt="Workation" />
-        <img
-          className="mt-6 rounded-lg shadow-lg"
-          src="/beach-work.jpg"
-          alt="Woman workationing on a beach"
-        />
+      <div className="grid lg:grid-cols-2 2xl:grid-cols-5">
+        <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
+          <div className="xl:max-w-xl">
+            <img className="h-10" src="/logo.svg" alt="Workcation" />
+            <img
+              className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+              src="/beach-work.jpg"
+              alt="Woman workcationing on the beach"
+            />
+            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+              You can work from anywhere.
+              <br className="hidden lg:inline" />{" "}
+              <span className="text-indigo-500">Take advantage of it.</span>
+            </h1>
+            <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+              Workcation helps you find work-friendly rentals in beautiful
+              locations so you can enjoy some nice weather even when you&apos;re
+              not on vacation.
+            </p>
+            <div className="mt-4 grid grid-cols-2 gap-4 sm:mt-6">
+              <a className="btn btn-primary text-center" href="#">
+                Book your escape
+              </a>
 
-        <h1 className="mt-6 text-2xl font-bold text-gray-900">
-          You can work from anywhere.{" "}
-          <span className="text-indigo-500">Take advantage of it.</span>
-        </h1>
-
-        <p className="mt-2 text-gray-600">
-          Workation helps you find work-friendly rentals in beautiful locations
-          so you can enjoy some nice weather even when you&apos;re not on
-          vacation
-        </p>
-
-        <div className="mt-4">
-          <a
-            className="inline-block px-4 py-2 rounded-lg shadow-lg uppercase tracking-wider font-semibold bg-blue-500 text-white text-sm"
-            href="#"
-          >
-            book you escape
-          </a>
+              <a className="btn btn-secondary text-center" href="#">
+                Ask our advice
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hidden relative lg:block 2xl:col-span-3">
+          <img
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src="/beach-work.jpg"
+            alt="Woman workcationing on the beach"
+          />
         </div>
       </div>
     </div>
